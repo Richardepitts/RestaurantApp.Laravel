98658b248eb21b3cdb92efc0b0e282b389894f6a
Richard Pitts <richardepitts@gmail.com>
2020-06-22 11:11:48 -0500

    3

--- app/Http/Controllers/Management/CategoryController.php
+++ app/Http/Controllers/Management/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view("");
+        return view('management.category');
     }
 
     /**
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('management.createCategory');
     }
 
     /**
---
+++ resources/views/management/category.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app');
+
+@section('content');
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+            
+                <div class="list-group">
+                    <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-align-left"></i>   Category</a>
+                    <a class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i>   Menu</a>
+                    <a class="list-group-item list-group-item-action"><i class="fas fa-chair"></i>   Table</a>
+                    <a class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i>  User</a>
+                </div>
+            
+            </div>
+            <div class="col-md-8">
+                <i class="fas fa-align-left"></i>
+            Category
+            <a href="/management/category/create" class="btn btn-success btn-sm float-right"><i class="fas fa-plus"></i>
+            Create Category</a>
+            </div>
+        </div>
+    </div>
+@endsection
