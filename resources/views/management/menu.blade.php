482e43c458971899770a1bd90daef090037df917
Richard Pitts <richardepitts@gmail.com>
2020-06-23 09:54:13 -0500

    1

--- resources/views/management/menu.blade.php
+++ resources/views/management/menu.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
       @include('management.inc.sidebar')
       <div class="col-md-8">
-        <i class="fas fa-hamburger"></i> Menu
+        <i class="fas fa-hamburger"></i>Menu
         <a href="/management/menu/create" class="btn btn-success btn-sm float-right"><i class="fas fa-plus"></i> Create Menu</a>
         <hr>
         @if(Session()->has('status'))
@@ -20,9 +20,6 @@
               <th scope="col">ID</th>
               <th scope="col">Name</th>
               <th scope="col">Price</th>
-              <td>
-                  <img src="{{asset('menu_images')}}/{{$menu->image}}" alt="{{$menu->name}}" width="120px" height="120px" class="img-thumbnail">
-                </td>
               <th scope="col">Picture</th>
               <th scope="col">Description</th>
               <th scope="col">Category</th>
@@ -31,7 +28,7 @@
             </tr>
           </thead>
           <tbody>
-          @foreach($menus as $menu)
+            @foreach($menus as $menu)
               <tr>
                 <td>{{$menu->id}}</td>
                 <td>{{$menu->name}}</td>
@@ -40,7 +37,7 @@
                   <img src="{{asset('menu_images')}}/{{$menu->image}}" alt="{{$menu->name}}" width="120px" height="120px" class="img-thumbnail">
                 </td>
                 <td>{{$menu->description}}</td>
-                <td>{{$menu->category->name}}</td>
+                <td>{{$menu->category_id}}</td>
                 <td><a href="/management/menu/{{$menu->id}}/edit" class="btn btn-warning">Edit</a></td>
                 <td>
                   <form action="/management/menu/{{$menu->id}}" method="post">
@@ -50,7 +47,7 @@
                   </form>
                 </td>
               </tr>
-            @endforeach  
+            @endforeach 
           </tbody>
         </table>
 
