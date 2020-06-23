06300636ac121d3d1f26387629bf01aa06694472
Richard Pitts <richardepitts@gmail.com>
2020-06-23 13:35:11 -0500

    !

--- app/Http/Controllers/Management/MenuController.php
+++ app/Http/Controllers/Management/MenuController.php
@@ -86,7 +86,9 @@ class MenuController extends Controller
      */
     public function edit($id)
     {
-        //
+        $menu = Menu::find($id);
+        $categories = Category::all();
+        return view('management.editMenu')->with('menu',$menu)->with('categories',$categories);
     }
 
     /**
@@ -98,7 +100,38 @@ class MenuController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // information validation
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'category_id' => 'required|numeric'
+        ]);
+        $menu = Menu::find($id);
+        // validate if a user upload image
+        if($request->image){
+            $request->validate([
+                'image' => 'nullable|file|image|mimes:jpeg,png,jpg|max:5000'
+            ]);
+            if($menu->image != "noimage.png"){
+                $imageName = $menu->image;
+                unlink(public_path('menu_images').'/'.$imageName);
+            }
+            $imageName = date('mdYHis').uniqid().'.'.$request->image->extension();
+            $request->image->move(public_path('menu_images'), $imageName);
+        }else{
+            $imageName = $menu->image;
+        }
+
+        $menu->name = $request->name;
+        $menu->price = $request->price;
+        $menu->image = $imageName;
+        $menu->description = $request->description;
+        $menu->category_id = $request->category_id;
+        $menu->save();
+        $request->session()->flash('status', $request->name. ' is updated successfully');
+        return redirect('/management/menu');
+
+
     }
 
     /**
@@ -109,6 +142,13 @@ class MenuController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $menu = Menu::find($id);
+        if($menu->image != "noimage.png"){
+            unlink(public_path('menu_images').'/'.$menu->image);
+        }
+        $menuName = $menu->name;
+        $menu->delete();
+        Session()->flash('status', $menuName. ' is deleted successfully');
+        return redirect('/management/menu');
     }
 }
