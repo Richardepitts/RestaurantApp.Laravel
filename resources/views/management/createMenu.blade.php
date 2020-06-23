76d49b569110fd9d60e0accc009305424203d449
Richard Pitts <richardepitts@gmail.com>
2020-06-23 07:52:21 -0500

    2

--- resources/views/management/createMenu.blade.php
+++ resources/views/management/createMenu.blade.php
@@ -1,34 +1,24 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
-@section('content');
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-            
-                <div class="list-group">
-                    <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-align-left"></i>   Category</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i>   Menu</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-chair"></i>   Table</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i>  User</a>
-                </div>
-            
-            </div>
-            <div class="col-md-8">
-                <i class="fas fa-hamburger"></i>
-            Create a Menu
-            <hr>
-            @if($errors-> any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li> {{ $error }} </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <form action="/management/menu" method="POST">
-                @csrf   
-                <div class="form-group">
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      @include('management.inc.sidebar')
+      <div class="col-md-8">
+        <i class="fas fa-hamburger"></i>Create a Menu
+        <hr>
+        @if($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+              </ul>
+          </div>
+        @endif
+        <form action="/management/menu" method="POST" enctype="multipart/form-data">
+          @csrf
+          <div class="form-group">
             <label for="menuName">Menu Name</label>
             <input type="text" name="name" class="form-control" placeholder="Menu...">
           </div>
@@ -58,15 +48,19 @@
             <input type="text" name="description" class="form-control" placeholder="Description...">
           </div>
 
+          <div class="form-group">
+            <label for="Category">Category</label>
+            <select class="form-control" name="category_id">
+              @foreach ($categories as $category)
+                <option value="{{$category->id}}">{{$category->name}}</option>
 
+              @endforeach
+            </select>
+          </div>
 
-
-          
-            
-                <button type="submit" class="btn btn-primary">Save</button>
-            </form>
-            <hr>
-            </div>
-        </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
     </div>
+  </div>
 @endsection
