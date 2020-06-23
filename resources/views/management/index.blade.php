fa93314f8c0deaa0de4ab5453cdf20f21a5259ce
Richard Pitts <richardepitts@gmail.com>
2020-06-22 21:42:02 -0500

    a

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -1,19 +1,11 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
-@section('content');
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-            
-                <div class="list-group">
-                    <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-align-left"></i>   Category</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i>   Menu</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-chair"></i>   Table</a>
-                    <a class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i>  User</a>
-                </div>
-            
-            </div>
-            <div class="col-md-8">Content</div>
-        </div>
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      @include('management.inc.sidebar')
+      <div class="col-md-8">
+      </div>
     </div>
+  </div>
 @endsection
