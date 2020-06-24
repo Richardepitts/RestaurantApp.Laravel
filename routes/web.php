a90471e5b3316098d0d1f33e1e5946a8c8325776
Richard Pitts <richardepitts@gmail.com>
2020-06-24 13:26:38 -0500

    1

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,5 @@ Route::get('/management', function(){
     Route::get('/report', 'Report\ReportController@index');
     Route::get('/report/show', 'Report\ReportController@show');
     
+    // Export to excel
+    Route::get('/report/show/export', 'Report\ReportController@export');
