cc81cab40fa1aeda097e84f72d0b7ab87c92aa0d
Richard Pitts <richardepitts@gmail.com>
2020-06-25 13:52:02 -0500

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Welcome</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                Richard's Restaurant App
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">You</a>
+                    <a href="https://laracasts.com">Make</a>
+                    <a href="https://laravel-news.com">Menus,</a>
+                    <a href="https://blog.laravel.com">Tables,</a>
+                    <a href="https://nova.laravel.com">and</a>
+                    <a href="https://forge.laravel.com">View</a>
+                    <a href="https://vapor.laravel.com">Reports:</a>
+                    <a href="https://github.com/laravel/laravel">Laravel/MySQL/PHP</a>
                 </div>
             </div>
         </div>
